1559d6cb27186b6191ee54d4b0f113d31b6a973c
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-07-12 11:50:07 +0300

    Added a click  event listener to a button.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+  const clickMe = document.querySelector("#button");
+  clickMe.addEventListener("click", clickAlert);
 }
+function clickAlert() {
+  alert("I was clicked!!");
+}
+
+addingEventListener();
